3360569e97791e9bb2cbed3064ea69fec9b19d33
Cataykus <cataykus@gmail.com>
2020-07-22 14:19:12 +0300

    sdelal refactoring

---
+++ Assets/Group1/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed = 2;
+    [SerializeField] private float _radiusToNewTarget = 4;
+
+    private Vector3 _target;
+
+    private void Start()
+    {
+        SetNewTarget();
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+
+        if (transform.position == _target)
+            SetNewTarget();
+    }
+
+    private void SetNewTarget()
+    {
+        _target = Random.insideUnitCircle * _radiusToNewTarget;
+    }
+}
---
+++ Assets/Group1/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _speedAbilityBoost;
+    [SerializeField] private float _speeedAbilityDuration;
+
+    public event UnityAction EnemyKilled;
+
+    void Update()
+    {
+        Move(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void Move(float x, float y)
+    {
+        transform.Translate(x * _speed * Time.deltaTime, y * _speed * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent(out Enemy enemy))
+        {
+            Destroy(enemy.gameObject);
+            EnemyKilled?.Invoke();
+            StartCoroutine(ActivateSpeedAbility());
+        }
+    }
+
+    private IEnumerator ActivateSpeedAbility()
+    {
+        _speed *= _speedAbilityBoost;
+        yield return new WaitForSeconds(_speeedAbilityDuration);
+        _speed /= _speedAbilityBoost;
+    }
+}
